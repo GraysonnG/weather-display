b6e8a28080d40abe1d7dcc653da7a2478f218f35
Blank <xinflux@gmail.com>
2026-05-23 18:40:30 -0400

    log the cat image index

--- src/routes/cat/cat.jsx
+++ src/routes/cat/cat.jsx
@@ -11,8 +11,9 @@ const imgStyle = {
 
 export const Cat = () => {
     const catImages = getAllImages("cats")
-
-    const imageToShow = catImages[Math.round(Math.random() * (catImages.length - 1))]
+    const index = Math.round(Math.random() * (catImages.length - 1))
+    console.log(`${index}/${catImages.length}`)
+    const imageToShow = catImages[index]
 
     return (<div style={{width: '100%', height: '100%', display: 'flex'}}>
         <img style={imgStyle} src={imageToShow} />
