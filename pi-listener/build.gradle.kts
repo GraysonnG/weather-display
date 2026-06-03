56fb7d74059697944a647c442de38ebeb97002f7
Blank <xinflux@gmail.com>
2026-06-02 21:29:27 -0400

    make simple web server

--- pi-listener/build.gradle.kts
+++ pi-listener/build.gradle.kts
@@ -1,22 +1,32 @@
 plugins {
     kotlin("jvm") version "2.3.21"
+    id("io.ktor.plugin") version "3.5.0"
+    application
 }
 
 group = "com.blanktheevil"
 version = "1.0-SNAPSHOT"
+
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+}
+
+application {
+    mainClass.set("MainKt")
 }
 
 kotlin {
     jvmToolchain(21)
 }
 
-tasks.test {
-    useJUnitPlatform()
+ktor {
+    fatJar {
+        archiveFileName.set("pi-listener.jar")
+    }
 }
---
+++ pi-listener/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
+fun notify(title: String, body: String) {
+    ProcessBuilder("notify-send", "--icon=network-group", "--urgency=normal", title, body)
+        .inheritIO()
+        .start()
+        .waitFor()
+}
+
+fun main() {
+    embeddedServer(Netty, port = 9876) {
+        routing {
+            post("/online") {
+                val time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+                notify("\uD83D\uDFE2 Raspberry Pi Online", "Ready at $time")
+                call.respondText("ok")
+            }
+        }
+    }.start(wait = true)
+}
