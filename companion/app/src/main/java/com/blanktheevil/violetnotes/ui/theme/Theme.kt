72ab69c0c4cb415704a0cf123f12049ef7628a07
Blank <xinflux@gmail.com>
2026-06-07 02:00:19 -0400

    companion: use colors from colors.xml instead of embedded in code

--- companion/app/src/main/java/com/blanktheevil/violetnotes/ui/theme/Theme.kt
+++ companion/app/src/main/java/com/blanktheevil/violetnotes/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.blanktheevil.violetnotes.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -10,27 +9,21 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import com.blanktheevil.violetnotes.R
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+@Composable
+private fun createDarkTheme() = darkColorScheme(
+    primary = colorResource(R.color.slate_primary_dark),
+    secondary = colorResource(R.color.slate_secondary_dark),
+    tertiary = colorResource(R.color.slate_tertiary_dark),
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+@Composable
+private fun createLightTheme() = lightColorScheme(
+    primary = colorResource(R.color.slate_primary_light),
+    secondary = colorResource(R.color.slate_secondary_light),
+    tertiary = colorResource(R.color.slate_tertiary_light),
 )
 
 @Composable
@@ -46,8 +39,8 @@ fun VioletNotesTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> createDarkTheme()
+        else -> createLightTheme()
     }
 
     MaterialTheme(
